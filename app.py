84e8a089689014e4f26829418c6b17d64adef340
saimeerar0608 <saimeerar0608@gmail.com>
2026-06-14 20:58:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
-model = joblib.load("churn_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = joblib.load(os.path.join(BASE_DIR, "churn_model.pkl"))
 
 
 @app.route('/')
